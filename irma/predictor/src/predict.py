2b6970e89c9265f50b8e1d2cc49f5d9f1dac6221
Alexandre CHETRIT <alexandre.chetrit@epitech.eu>
2020-05-28 16:05:10 +0200

    feat: changing normalization and denormalization

--- irma/predictor/src/predict.py
+++ irma/predictor/src/predict.py
@@ -29,33 +29,32 @@ def init_data(stocks_path):
     close_data = numpy.asarray(close_data)
     return (open_data, close_data)
 
-def normalize_data(x_array, y_array):
-    x_array = numpy.divide(
-                    numpy.subtract(x_array, numpy.mean(x_array)),
-                    numpy.nanstd(x_array)
-                )
-    y_array = numpy.divide(
-                    numpy.subtract(y_array, numpy.mean(y_array)),
-                    numpy.nanstd(y_array)
-                )
+def normalize_data(x_array, y_array, scaler) :
+    x_array = numpy.reshape(x_array, (-1, 1))
+    x_array = scaler.fit_transform(x_array)
+    x_array = numpy.ndarray.flatten(x_array)
+
+    y_array = numpy.reshape(y_array, (-1, 1))
+    y_array = scaler.fit_transform(y_array)
+    y_array = numpy.ndarray.flatten(y_array)
     return (x_array, y_array)
 
-def denormalize_data(x_array, y_array):
-    x_array = numpy.divide(
-                    numpy.nanstd(x_array),
-                    numpy.subtract(x_array, numpy.mean(x_array))
-                )
-    y_array = numpy.divide(
-                    numpy.nanstd(y_array),
-                    numpy.subtract(y_array, numpy.mean(y_array))
-                )
-    return x_array, y_array
+def denormalize_data(x_array, y_array, scaler) :
+    x_array = numpy.reshape(x_array, (-1, 1))
+    x_array = scaler.inverse_transform(x_array)
+    x_array = numpy.ndarray.flatten(x_array)
+    x_array = numpy.ndarray.astype(x_array, int)
+
+    y_array = numpy.reshape(y_array, (-1, 1))
+    y_array = scaler.inverse_transform(y_array)
+    y_array = numpy.ndarray.flatten(y_array)
+    y_array = numpy.ndarray.astype(y_array, int)
+    return (x_array, y_array)
 
 def create_model():
     model = tensorflow.keras.models.Sequential()
     model.add(tensorflow.keras.layers.Dense(5, input_shape=(1,)))
     model.add(tensorflow.keras.layers.Dense(1, input_shape=(1,)))
-
     ##input_layer = tensorflow.keras.layers.Input((1, ))
     ##dense1_layer = tensorflow.keras.layers.Dense(units=1, input_shape=(1,))
     ##output = dense1_layer(input_layer)
@@ -75,13 +74,14 @@ def print_predict(predict, real):
     matplotlib.pyplot.show()
 
 def predict_on_stocks(stocks_path: str, store_path: str, models_path: str):
+    scaler = StandardScaler()
     open_data, close_data = init_data(stocks_path)
-    open_data, close_data = normalize_data(open_data, close_data)
+    open_data, close_data = normalize_data(open_data, close_data, scaler)
     x_train, x_test, y_train, y_test = train_test_split(open_data, close_data, test_size=0.10, random_state=42)
 
     model = create_model()
     model.fit(x_train, y_train, epochs=EPOCHS)
     predicted_data = model.predict(x_test)
     real_data = y_test
-    #predicted_data, real_data = denormalize_data(predicted_data, real_data)
+    #predicted_data, real_data = denormalize_data(predicted_data, real_data, scaler)
     print_predict(predicted_data, real_data)
