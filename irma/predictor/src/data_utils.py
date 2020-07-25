2e0406eab25e780fec85c0fb840fb4cc86622c82
Alexandre CHETRIT <alexandre.chetrit@epitech.eu>
2020-07-25 20:23:40 +0200

    feat: ia improvements, code visibility improvements, fixing nan bug, remake of main function

--- irma/predictor/src/data_utils.py
+++ irma/predictor/src/data_utils.py
@@ -4,37 +4,27 @@ import math
 # Tensorflow
 import tensorflow
 
-# Globals
-NB_INDICATORS = 15
-BATCH_SIZE = 9
-
-def have_one_nan(array: numpy.array, line: int):
-    for i in range(NB_INDICATORS):
-        if math.isnan(array[line][i]) is True:
-            return True
-    return False
-
 def init_data(array):
     close_data = []
-    for i in range(len(array)):
+    length = len(array)
+    i = 0
+    while i != length:
         if i >= len(array):
-            break
-        if have_one_nan(array, i) is True:
+            i += 1
+            continue
+        if math.isnan(numpy.min(array[i])) is True:
             array = numpy.delete(array, i, axis=0)
             i -= 1
-    #print(array)
+        i += 1
     for i in range(len(array)):
         close_data.append(array[i][4])
     close_data = numpy.asarray(close_data)
     array = numpy.delete(array, 4, 1)
     return (array, close_data)
-#print(array.shape)
-#print(close_data.shape)
-#print('array:', array)
-#print('close_data:', close_data)
 
-def normalize_data(x_array, y_array, scaler):
-    x_array = numpy.reshape(x_array, (-1, NB_INDICATORS - 1))
+
+def normalize_data(x_array, y_array, scaler, nb_indicators):
+    x_array = numpy.reshape(x_array, (-1, nb_indicators - 1))
     x_array = scaler.fit_transform(x_array)
 
     y_array = numpy.reshape(y_array, (-1, 1))
@@ -58,20 +48,6 @@ def split_data(open_data: numpy.array, close_data: numpy.array):
     x_test = open_data[int(len(open_data) * 0.9) : len(open_data)]
     y_test = close_data[int(len(close_data) * 0.9) : len(close_data)]
     return x_train, y_train, x_test, y_test
-
-def batch_data(x_train: numpy.array, y_train: numpy.array):
-
-    dataset = tensorflow.data.Dataset.from_tensor_slices((x_train, y_train))
-    dataset = dataset.batch(BATCH_SIZE)
-    (shuffle_open_data, shuffle_close_data) = list(zip(*list(dataset.as_numpy_iterator())))
-    return numpy.array(shuffle_open_data), numpy.array(shuffle_close_data)
-
-def unbatch_data(x_train: numpy.array, y_train: numpy.array):
-
-    dataset = tensorflow.data.Dataset.from_tensor_slices((x_train, y_train))
-    dataset = dataset.unbatch()
-    (shuffle_open_data, shuffle_close_data) = list(zip(*list(dataset.as_numpy_iterator())))
-    return numpy.array(shuffle_open_data), numpy.array(shuffle_close_data)
 
 def shuffle_data(x_train: numpy.array, y_train: numpy.array):
 
--- irma/predictor/src/predict.py
+++ irma/predictor/src/predict.py
@@ -19,69 +19,55 @@ from data_utils import *
 
 EPOCHS = 20
 NB_INDICATORS = 15
-#BATCH_SIZE = 9
 
-def create_model(x_train):
+def create_model(model_path):
     model = tensorflow.keras.models.Sequential()
-
-    #model.add(tensorflow.keras.layers.LSTM(256))
-    #model.add(tensorflow.keras.layers.Dropout(0.2))
     model.add(tensorflow.keras.layers.Dense(256))
     model.add(tensorflow.keras.layers.Dense(128))
     model.add(tensorflow.keras.layers.Dense(64))
     model.add(tensorflow.keras.layers.Dense(1))
+
     model.compile(
         loss="mean_squared_error",
-        optimizer=tensorflow.keras.optimizers.Adam(0.1),
+        optimizer=tensorflow.keras.optimizers.Adam(lr=1e-3, decay=1e-4),
     )
-    #TenserBoard
-    log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = tensorflow.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-    #EndTenserBoard
-    return (model, tensorboard_callback)
 
-def print_predict(predict, real, x_test):
+    #Checkpoint model
+    checkpoint_callback = tensorflow.keras.callbacks.ModelCheckpoint(
+        model_path, monitor='val_loss', mode='min', save_best_only=True,
+        verbose=1, save_weights_only=False, save_freq='epoch'
+    )
+
+    return (model, checkpoint_callback)
+
+def print_predict(predict, real, x_test, interval):
     """printing predict"""
-    '''use x_test in plot if you want the comparative plot'''
     matplotlib.pyplot.plot(predict, 'r')
     matplotlib.pyplot.plot(real, 'b')
     matplotlib.pyplot.title('Predict')
-    matplotlib.pyplot.xlabel('Days')
+    matplotlib.pyplot.xlabel(f'interval {interval}')
     matplotlib.pyplot.ylabel('Close Price')
     matplotlib.pyplot.show()
 
-def test_prediction(model, x_test, y_test, scaler):
-    #print(x_test.shape, y_test.shape)
+def test_model(model, x_test, y_test, scaler, interval):
     predicted_data = model.predict(x_test)
-    #print(predicted_data.shape)
-    #(x_test, y_test) = unbatch_data(x_test, y_test)
     predicted_data, real_data = denormalize_data(predicted_data, y_test, scaler)
-    x_test, x_test = denormalize_data(x_test, x_test, scaler)
-    #print(predicted_data.shape, real_data.shape, x_test.shape)
-    print_predict(predicted_data, real_data, x_test)
+    x_test = denormalize_data(x_test, x_test, scaler)
+    print_predict(predicted_data, real_data, x_test, interval)
 
 
-def predict_on_stocks(array: numpy.array, store_path: str, models_path: str):
+def predict_on_stocks(array: numpy.array, model_path: str, interval: str):
     scaler = StandardScaler()
     open_data, close_data = init_data(array)
 
-    open_data, close_data = normalize_data(open_data, close_data, scaler)
-
+    open_data, close_data = normalize_data(open_data, close_data, scaler, NB_INDICATORS)
     (x_train, y_train, x_test, y_test) = split_data(open_data, close_data)
     (x_train, y_train) = shuffle_data(x_train, y_train)
-    #(x_train, y_train) = batch_data(x_train, y_train)
-    #(x_test, y_test) = batch_data(x_test, y_test)
 
-    #print('ints:', len(x_train), len(x_test))
-    #x_train = numpy.reshape(x_train, (int(len(x_train) / 2), BATCH_SIZE, NB_INDICATORS - 1))
-    #x_test = numpy.reshape(x_test, (int(len(x_test) / 2), BATCH_SIZE, NB_INDICATORS - 1))
-    #print('shapes:', x_train.shape, x_test.shape)
-    #y_train = numpy.reshape(y_train, (-1, BATCH_SIZE))
-    #y_test = numpy.reshape(y_test, (-1, BATCH_SIZE))
+    (model, checkpoint_callback) = create_model(model_path)
+    model.fit(
+        x_train, y_train, validation_data=(x_test, y_test), batch_size=64,
+        epochs=EPOCHS, callbacks=[checkpoint_callback]
+    )
 
-    #print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
-    '''in this function, the model takes NB_INDICATORS - 1 columns
-    and try to predict one data (in this spot the close price of the daily candle)'''
-    model, tensorboard_callback = create_model(x_train)
-    model.fit(x_train, y_train, batch_size=128, epochs=EPOCHS, callbacks=[tensorboard_callback])
-    test_prediction(model, x_test, y_test, scaler)
+    #test_model(model, x_test, y_test, scaler, interval)
