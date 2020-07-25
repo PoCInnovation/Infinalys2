f8c1da601e781f4c3351345e42a9104be5932322
brasseur <gregoire.brasseur@epitech.eu>
2020-07-25 16:06:55 +0200

    feat: add Chart module for frontend

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,34 @@
 import React from 'react';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Chart from './Chart_candle';
+import { getData } from "./utils"
+import { TypeChooser } from "react-stockcharts/lib/helper";
 import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
+
+
+//MARK: this way is for chart component
+class ChartComponent extends React.Component {
+	componentDidMount() {
+		getData().then(data => {
+			this.setState({ data })
+		})
+	}
+	render() {
+		if (this.state == null) {
+			return <div>Loading...</div>
+		}
+		return (
+			<TypeChooser>
+				{type => <Chart type={type} data={this.state.data} />}
+			</TypeChooser>
+		)
+	}
+}
+
 
 function App() {
   const [state, setState] = React.useState({
@@ -15,17 +43,11 @@ function App() {
   };
 
   return(
-    <FormControlLabel
-      control = {
-        <Checkbox
-          checked = {state.Boll}
-          onChange = {handleChange}
-          name = 'Boll'
-          color = 'primary'
-        />
-      }
-      label = "Boll"
-      />
+    <div>
+      <ChartComponent />
+
+      
+    </div>
   );
 }
 
