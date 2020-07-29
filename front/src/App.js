bbb0923163804723db3ed66ebd54cdea3d158551
brasseur <gregoire.brasseur@epitech.eu>
2020-07-29 09:59:34 +0200

    feat: dark mode and cleaning code

--- front/src/App.js
+++ front/src/App.js
@@ -7,6 +7,12 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+// import ButtonAppBar from './AppBar'
+
+
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 //MARK: this way is for chart component
 class ChartComponent extends React.Component {
@@ -28,7 +34,20 @@ class ChartComponent extends React.Component {
 }
 
 
+
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   const [state, setState] = React.useState({
     Boll: false,
   });
@@ -36,37 +55,41 @@ function App() {
   const handleChange = (event) => {
     setState({ ...state, [event.target.name]: event.target.checked });
   };
+
+
   return(
     <div>
-      
-      <ChartComponent boolinger={state.Boll}/>
-      <FormControlLabel
-        control = {
-          <Checkbox
-            checked = {state.Boll}
-            onChange = {handleChange}
-            name = 'Boll'
-            color = 'primary'
-          />
-        }
-        label = "Boll"
-      />
-      <FormControl>
-        <InputLabel>Boursse</InputLabel>
-        <Select
-          native
-          value={state.age}
-          //onChange={handleChange}
-          inputProps={{
-            name: 'Boursse',
-          }}
-        >
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <ChartComponent boolinger={state.Boll}/>
+        <FormControlLabel
+          control = {
+            <Checkbox
+              checked = {state.Boll}
+              onChange = {handleChange}
+              name = 'Boll'
+              color = 'primary'
+            />
+          }
+          label = "Boll"
+        />
+        <FormControl>
+          <InputLabel>Boursse</InputLabel>
+          <Select
+            native
+            value={state.age}
+            //onChange={handleChange}
+            inputProps={{
+              name: 'Boursse',
+            }}
+          >
           <option aria-label="None" value="" />
           <option value={() => console.log("TESLA")}>TESLA</option>
           <option value={() => console.log("APPL")}>APPL</option>
           <option value={() => console.log("BTC")}>BTC-USD</option>
-        </Select>
-      </FormControl>
+          </Select>
+        </FormControl>
+      </ThemeProvider>
     </div>
   );
 }
--- front/src/utils.js
+++ front/src/utils.js
@@ -2,6 +2,7 @@ import { tsvParse} from "d3-dsv";
 import { timeParse } from "d3-time-format";
 
 function parseData(parse) {
+	console.log(parse)
 	return function(d) {
 		d.date = parse(d.date);
 		d.open = +d.open;
@@ -10,15 +11,20 @@ function parseData(parse) {
 		d.close = +d.close;
 		d.volume = +d.volume;
 
+		console.log(d)
 		return d;
 	};
 }
 
 const parseDate = timeParse("%Y-%m-%d");
 
+//https://cdn.rawgit.com/rrag/react-stockcharts/master/docs/data/MSFT.tsv 
+//http://0.0.0.0:5000/?stock=AAPL&interval=3mo
+
 export function getData() {
 	const promiseMSFT = fetch("https://cdn.rawgit.com/rrag/react-stockcharts/master/docs/data/MSFT.tsv")
 		.then(response => response.text())
 		.then(data => tsvParse(data, parseData(parseDate)))
+		console.log(promiseMSFT)
 	return promiseMSFT;
 }
