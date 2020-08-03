77301e2be0decb6de50dd8baccd6a9988356d996
coline <coline.seguret@epitech.eu>
2020-08-03 15:25:18 +0200

    feat: html form backend

--- irma/predictor/src/web.py
+++ irma/predictor/src/web.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request, send_file, render_template
 from sklearn.preprocessing import StandardScaler
 from joblib import load
+
+from flask_wtf import FlaskForm
+from wtforms import SelectField
 
 import tensorflow
 import numpy
@@ -15,6 +18,8 @@ from data_utils import normalize_data
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'secret'
+
 HOST = "0.0.0.0"
 PORT = 8080
 DEBUG = True
@@ -22,6 +27,9 @@ DEBUG = True
 MODELS_PATH = "../models"
 TSV_PATH = "../tsv"
 NB_INDICATORS = 15
+
+class Form(FlaskForm):
+    interval = SelectField('interval', choices=['1m', '2m', '5m', '15m', '30m', '60m', '90m','1h', '1d', '5d', '1wk', '1mo', '3mo'])
 
 def prepare_prediction(array: numpy.array):
     scaler = StandardScaler()
@@ -32,14 +40,6 @@ def prepare_prediction(array: numpy.array):
 
     return (open_data)
 
-def transform_nan(stocks_data: numpy.array):
-    stocks_data = stocks_data.tolist()
-    for y in range(len(stocks_data)):
-        for i in range(len(stocks_data[y])):
-            if math.isnan(stocks_data[y][i]):
-                stocks_data[y][i] = None
-    return (numpy.array(stocks_data))
-
 def get_interval():
     interval = None
     if request.args.get("interval") is None:
@@ -48,37 +48,43 @@ def get_interval():
         interval = str(request.args["interval"])
     return (interval)
 
-@app.route("/", methods=["GET"])
+def prediction_ia(stock_symbol, interval, model_path):
+    stocks_data = generate_model_on_stock(stock_symbol, interval)
+    scaler = load(f'{model_path}/std_scaler.bin')
+    model = tensorflow.keras.models.load_model(model_path)
+
+    prediction = predict_one_interval(
+        model,
+        numpy.array(prepare_prediction(stocks_data)),
+        scaler
+    )
+    return (prediction)
+
+def get_info(stock_symbol, interval):
+    model_path = f'{MODELS_PATH}/model_{stock_symbol}_{interval}'
+
+    prediction = prediction_ia(stock_symbol, interval, model_path)
+
+    prediction_list = [0, 0, 0, 0, 0, 0]
+    prediction_list[4] = prediction[0]
+
+    list_to_tsv(stock_symbol, interval, prediction_list)
+
+@app.route("/", methods=["GET", "POST"])
 def backend():
     interval = get_interval()
-
-    if request.args.get('stock') is not None and request.args.get('interval') is not None:
+    form = Form()
+    if request.args.get('stock') is not None:
         stock_symbol = str(request.args['stock'])
-        model_path = f'{MODELS_PATH}/model_{stock_symbol}_{interval}'
-
-        stocks_data = generate_model_on_stock(stock_symbol, interval)
-        scaler = load(f'{model_path}/std_scaler.bin')
-        model = tensorflow.keras.models.load_model(model_path)
-
-        prediction = predict_one_interval(
-            model,
-            numpy.array(prepare_prediction(stocks_data)),
-            scaler
-        )
-
-        stocks_data = transform_nan(
-            numpy.delete(
-                stocks_data, numpy.s_[7:NB_INDICATORS - 1], axis=1
-            )
-        ).tolist()
-
-        prediction_list = [0, 0, 0, 0, 0, 0]
-        prediction_list[4] = prediction[0]
-
-        list_to_tsv(stock_symbol, interval, prediction_list)
+        get_info(stock_symbol, interval)
         return (send_file(f'{TSV_PATH}/{stock_symbol}_{interval}.tsv', as_attachment=True))
-    else:
-        return jsonify('Please enter a stock symbol and an interval in the URL')
+    try:
+        stock_symbol = request.form.get('Stock')
+        interval = request.form.get('interval')
+        get_info(stock_symbol, interval)
+        return (send_file(f'{TSV_PATH}/{stock_symbol}_{interval}.tsv', as_attachment=True))
+    except:
+        return render_template('index.html', form=form)
 
 if __name__ == "__main__":
     app.run(host=HOST, debug=DEBUG)
