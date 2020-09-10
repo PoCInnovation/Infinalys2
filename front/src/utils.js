1effb735321c2414a6b315990a38fd085d35765c
Paul Monnery <paul.monnery@epitech.eu>
2020-09-10 13:52:17 +0200

    feat: reverted abck to serve for docker compose

--- front/src/utils.js
+++ front/src/utils.js
@@ -6,19 +6,11 @@ const parseDate = timeParse('%Y-%m-%d');
 function parseData(parse) {
   return function (d) {
     d.date = parseDate(d.Date);
-    // var curentday = new Date()
-    // if (curentday < d.date) {
-    // 	console.log(curentday)
-    // 	console.log(d.date)
-    // }
     d.open = +d.Open;
     d.high = +d.High;
     d.low = +d.Low;
     d.close = +d.Close;
     d.volume = +d.Volume;
-    // if (d.volume === 0) {
-    //   console.log(d);
-    // }
     return d;
   };
 }
@@ -27,7 +19,7 @@ function parseData(parse) {
 // http://0.0.0.0:5000/?stock=AAPL&interval=3mo
 
 export default function getData(trade, day) {
-  const promiseMSFT = fetch(`http://${process.env.NODE_ENV === 'production' ? 'back' : '0.0.0.0'}:5000/?stock=${trade}&interval=${day}`)
+  const promiseMSFT = fetch(`http://0.0.0.0:5000/?stock=${trade}&interval=${day}`)
     .then((response) => response.text())
     .then((data) => tsvParse(data, parseData(parseDate)));
   return promiseMSFT;
